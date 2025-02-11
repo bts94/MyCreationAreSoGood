5deda02edd9f1157ef4577718bba7d1a5ce62028
Orest Tsyaputa <orest.tsyaputa@mc2innovations.com>
2025-02-11 18:22:10 +0100

    feature 66qw32tt1  and more stuff here for sure

---
+++ public/This.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+console.log('123')
+</script>
+
+<template>
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores eos quae ullam! Consequatur corporis dignissimos distinctio, eaque exercitationem magnam magni reiciendis saepe. Adipisci aliquam asperiores blanditiis, consequatur cumque, eius ex fuga inventore maiores minus molestias neque, officia quasi rem sapiente sequi sunt. Eligendi eum minus porro quidem voluptas voluptate? Accusamus deleniti illum libero maxime nam natus possimus rerum sit veniam. Aliquid doloremque eaque, magni maxime molestiae, nesciunt odio optio, quo reprehenderit soluta suscipit voluptatem? Assumenda at dicta distinctio, dolor eius facilis in laborum maiores molestias porro praesentium repellendus. A commodi consequuntur deserunt distinctio dolorem eius eos illo iure laboriosam sed!</div>
+</template>
+
+<style scoped>
+dialog {
+  background: #000;
+  color: #000;
+}
+</style>
